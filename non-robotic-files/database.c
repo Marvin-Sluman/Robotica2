1be0782ecd4924f02de2a78ebd08876b2afaaf63
Marvin Sluman <mar.sluman@gmail.com>
2022-09-27 13:23:34 +0200

    More dynamic

--- non-robotic-files/database.c
+++ non-robotic-files/database.c
@@ -1,8 +1,11 @@
 #include <mariadb/mysql.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 char option;
+char input[255];
+char addInput[15][50];
 char query[300];
 char driverRef[255];
 int number;
@@ -13,15 +16,11 @@ char date[20];
 char nationality[255];
 char url[255];
 
+char columns[15][50];
+
 MYSQL *con;
 MYSQL_RES *result;
 MYSQL_ROW row;
-
-char get_option(){
-  printf("What do you want to do? Add (A), Modify (M), Delete (D) or View (V)? : ");
-  scanf("%c", &option);
-  return option;
-}
 
 void finish_with_error(MYSQL *con){
   printf("%s\n", mysql_error(con));
@@ -29,7 +28,69 @@ void finish_with_error(MYSQL *con){
   exit(1);
 }
 
+void show_tables(){
+  if(mysql_query(con, "SHOW TABLES")){
+    finish_with_error(con);
+  }
+  result = mysql_store_result(con);
+  if(result == NULL){
+    finish_with_error(con);
+  }
+
+  while((row = mysql_fetch_row(result))){
+      printf("%s ", row[0]); 
+    printf("\n");
+  }
+  printf("\n");
+  mysql_free_result(result);
+}
+
+void show_columns(){
+  sprintf(query, "SHOW COLUMNS FROM %s", input);
+  if(mysql_query(con, query)){
+  }
+  result = mysql_store_result(con);
+  if(result == NULL){
+    finish_with_error(con);
+  }
+
+  for(int i = 0; (row = mysql_fetch_row(result)); i++){
+      strcpy(columns[i], row[0]);
+      printf("%s ", row[0]); //Not interested in columns information except the column name
+    printf("\n");
+  }
+  printf("\n");
+  mysql_free_result(result);
+} 
+
+char get_option(){
+  printf("What do you want to do? Add (A), Modify (M), Delete (D) or View (V)? : ");
+  scanf(" %c", &option);
+  return option;
+}
+
 void add_data(){
+  sprintf(query, "INSERT INTO %s (%s", input, columns[0]);
+  for(int i = 1; i < 9; i++){
+    printf("%s  ", columns[i]); //Ignore i=0, because it's auto incrementing ID
+    strcat(query, ", ");
+    strcat(query, columns[i]);
+    scanf("%s", addInput[i-1]);
+  }
+  strcat(query, ") VALUES (NULL");
+  for(int i = 1; i < 9; i++){
+      strcat(query, ", '");
+      strcat(query, addInput[i-1]);
+      strcat(query, "'");
+  }
+  strcat(query, ")");
+
+  printf("\n%s\n", query);
+  if(mysql_query(con, query)){
+    finish_with_error(con);
+  }
+
+  /*
   printf("driverRef ");
   scanf("%s", driverRef);
   printf("number ");
@@ -50,6 +111,7 @@ void add_data(){
   if(mysql_query(con, query)){
     finish_with_error(con);
   }
+  */
 }
 
 void delete_data(){
@@ -94,7 +156,15 @@ int main(int argc, char **argv)
     printf("%s\n", mysql_error(con));
     mysql_close(con);
     exit(1);
-  }
+  } 
+
+  show_tables();
+  
+  printf("Please select the table you want to use: ");
+  scanf("%s", input);
+  printf("\n");
+
+  show_columns();
 
   switch (get_option()){
     case 'A':
