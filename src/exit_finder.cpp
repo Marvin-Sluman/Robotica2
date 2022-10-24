4605054c679e827a28908c3fcfb8eddfbcdc6d3d
Marvin Sluman <mar.sluman@gmail.com>
2022-10-24 12:03:46 +0200

    Working exit finder

--- src/exit_finder.cpp
+++ src/exit_finder.cpp
@@ -2,8 +2,13 @@
 #include "robot.h"
 
 Servo servo;
-int distance[3];
+int distance;
 int bottomSensor;
+unsigned long beginTime;
+unsigned long currentTime;
+unsigned long duration;
+unsigned long returnDuration;
+uint8_t sensorValue[4]; 
 
 void setup(){
     Serial.begin(9600);
@@ -13,46 +18,49 @@ void setup(){
     pinMode(PIN_TRACKING_RIGHT, INPUT); 
     pinMode(PIN_TRACKING_CENTER, INPUT);
     servo.attach(PIN_SERVO);
+    delay(6000);
 }
 
 void loop(){
-    servo.write(10);
-    distance[0] = getSonar(); 
-    delay(200);
     servo.write(90);
-    distance[1] = getSonar(); 
-    delay(200);
-    servo.write(170);
-    distance[2] = getSonar();
-    delay(200); 
-    
-    Serial.print("Left: ");
-    Serial.print(distance[0]);
-    Serial.print("   Center: ");
-    Serial.print(distance[1]);
-    Serial.print("   Right: ");
-    Serial.print(distance[2]);
-    Serial.println();
-
-    if(distance[1] > distance[0] && distance[1] > distance[2]){
-        while(1){
-            drive(80, 80);
-            bottomSensor = getLightSensor();
-            /*if (bottomSensor > 0){
-                exit(1);
-            }else{*/
-                servo.write(90);
-                if(getSonar() <10){
-                    delay(300);
-                    drive(0, 0);
-                    return;
-                } 
-                delay(200);
-            //}
-        }
-    }else if(distance[0] > distance[2]){
-        drive(80, -80);
-    }else{
+    distance = getSonar(); 
+    if(distance < 50){
+        Serial.println(distance);
         drive(-80, 80);
+        delay(30);
+    }else{
+        drive(0, 0);
+        delay(3000);
+        drive(-80, 80);
+        delay(200);
+        drive(0, 0);
+        beginTime = millis();
+        drive(80, 80);
+        while(1){
+            sensorValue[0] = digitalRead(PIN_TRACKING_LEFT);
+            sensorValue[1] = digitalRead(PIN_TRACKING_CENTER);
+            sensorValue[2] = digitalRead(PIN_TRACKING_RIGHT);
+            sensorValue[3] = sensorValue[0] << 2 | sensorValue[1] << 1 | sensorValue[2]; //Moves the bits to create different values for the combinations of sensors
+            if(sensorValue[3] > 0){
+                delay(500);
+                drive(0,0);
+                currentTime = millis();
+                duration = currentTime - beginTime;
+                //Turn 180 degrees
+                drive(-100, 100);
+                delay(1400);
+                drive(0, 0);
+                delay(500);
+                beginTime = millis();
+                drive(80, 80);
+                while(returnDuration < duration){
+                    currentTime = millis();
+                    returnDuration = currentTime - beginTime;
+                }
+                drive(0,0);
+                delay(60000);
+            }
+        }
     }
+
 }
